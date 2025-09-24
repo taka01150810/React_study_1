000690f6693eca0a3f831bd37f5bbf8706bddeb4
宮中良明 <taka01150810@gmail.com>
2025-09-24 23:02:33 +0900

    65. create-react-app（create-react-appのインストール）

---
+++ sec06/my-app/src/App.jsx
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Reactテスト中です。
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://tanstack.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn TanStack
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
+
---
+++ sec06/my-app/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [viteReact()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  }
+});
