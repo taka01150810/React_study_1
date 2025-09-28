3f9656cc4b09d2c936e23a2fe4f198017d14ae3c
宮中良明 <taka01150810@gmail.com>
2025-09-28 15:53:49 +0900

    83.  react-router-dom（プログラムで移動する useNavigate()）

--- sec07/router-test/src/pages/Information.js
+++ sec07/router-test/src/pages/Information.js
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom'
+
 const Information = () => {
+  const navigate = useNavigate()
+
+  const goToContact = () => navigate('/contact')
 
   return (<>
   <h2>Informationです。</h2>
+  <button onClick={goToContact}>お問合せページ</button>
   </>)
 }
 
