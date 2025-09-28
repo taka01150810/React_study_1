6c40e292c02f472f55577f474c438204375a30f1
宮中良明 <taka01150810@gmail.com>
2025-09-28 15:46:10 +0900

    80. react-router-dom（BrowserRouter, Routes, Route）

--- sec07/router-test/src/App.js
+++ sec07/router-test/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Information from './pages/Information';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Routesで切り替えたいRouteを囲む */}
+      <Routes>
+        {/* Routeでコンポーネントを指定することができる */}
+        {/* ※ver5ではRoutesではなくSwitchを使用する */}
+        <Route path="/" element={<Home />} />
+        <Route path="/information" element={<Information />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
