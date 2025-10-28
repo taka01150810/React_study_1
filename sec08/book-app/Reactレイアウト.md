e34ff75778151e9cf290791dfeab44d0e893929a
宮中良明 <taka01150810@gmail.com>
2025-10-28 17:32:54 +0900

    91. React レイアウト Box, Container, Grid

---
+++ sec08/book-app/Reactレイアウト.md
@@ -0,0 +1,8 @@
+muiのComponents/LAYOUT
+Box 基本的にこれを使う。
+ compoment html要素を設定
+Container 要素をセンターに寄せる
+ maxWidth 最大幅設定
+Grid cssのflexbox
+ Grid containerの中にGrid item
+ xsやmdなどを設定する事でブラウザの幅に応じて表示を変更できる。(12列で考える。breakpointも参照)
--- sec08/book-app/src/pages/MuiTest.js
+++ sec08/book-app/src/pages/MuiTest.js
@@ -1,9 +1,13 @@
 import MuiButton from "../components/MuiButton"
+import SignIn from "../components/SignIn"
+import MuiThemeTest from "../components/MuiThemeTest"
 
 const MuiTest = () => {
 
   return (<>
     <MuiButton />
+    <MuiThemeTest />
+    <SignIn />
   </>)
 }
 
