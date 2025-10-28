f4bd252e2edd49a36fd230a252a70ee3bfdf0ae2
宮中良明 <taka01150810@gmail.com>
2025-10-28 17:39:17 +0900

    削除

--- sec08/book-app/src/pages/MuiTest.js
+++ sec08/book-app/src/pages/MuiTest.js
@@ -1,12 +1,10 @@
 import MuiButton from "../components/MuiButton"
 import SignIn from "../components/SignIn"
-import MuiThemeTest from "../components/MuiThemeTest"
 
 const MuiTest = () => {
 
   return (<>
     <MuiButton />
-    <MuiThemeTest />
     <SignIn />
   </>)
 }
