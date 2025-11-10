0be172c47abfcb8a26d3dfcc351d1dfa6f90875d
宮中良明 <taka01150810@gmail.com>
2025-11-10 22:16:30 +0900

    101. React 検索画面: BookSearchのCard表示

--- sec08/book-app/src/pages/books/BookSearch.js
+++ sec08/book-app/src/pages/books/BookSearch.js
@@ -5,10 +5,16 @@ import {
   TextField,
   Button,
   Typography,
-  Box
+  Box,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions
 } from '@mui/material'
 import { Link } from 'react-router-dom'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import AddIcon from '@mui/icons-material/Add'
 
 const BookSearch = ({ books, setBooks }) => {
   const keyword = useRef('')
@@ -85,6 +91,41 @@ const BookSearch = ({ books, setBooks }) => {
             </Button>
           </Box>
         </Box>
+      </Container>
+      <Container component="section" maxWidth="lg">
+        <Grid container spacing={4}>
+          { searchResult.map((card, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4} >
+              <Card sx={{ height: '100%'}}>
+                <Grid container>
+                  <Grid item sm={4}>
+                    <CardMedia
+                      component="img"
+                      image={card.image}
+                      alt={card.title}
+                      />
+                  </Grid>
+                  <Grid item sm={8}>
+                    <CardContent>
+                      <Typography sx={{ fontSize: '16px'}}>
+                        { card.title}
+                      </Typography>
+                      <Typography sx={{ fontSize:'14px', mb: 1.5 }}
+                      color="text.secondary">
+                        { card.description }
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Fab color="primary">
+                        <AddIcon />
+                      </Fab>
+                    </CardActions>
+                  </Grid>
+                </Grid>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </>
   )
